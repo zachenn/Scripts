8e8483dc23618ac01d005638b1658f7246a076d8
Zach Chen <myemail757@gmail.com>
2019-05-13 14:12:03 -0700

    Added clarity to script structure

--- Customer Analysis (Python)/customerList.py
+++ Customer Analysis (Python)/customerList.py
@@ -6,21 +6,29 @@ from datetime import datetime       # to convert unix timestamp to readable date
 from lxml import html               # to parse html
 from bs4 import BeautifulSoup       # to parse html
 
+'''
+you can run the script in 1 of 2 ways. Both return a CSV file for additional analysis
+1) use an HTTPS request. use getApplicationIDsFromAPI()
+2) use a CSV file. use getApplicationIDsFromCSV()
 
-# download csv file. run it through the script, and you should have a csv file back with new
-# information that you can view with excel
+note: using an HTTPS request w/ a single ID vs using a time frame
+returns different JSON responses
 
-def getApplicationIDsFromAPI(): # 200 day max
+'''
+
+# using an HTTPS request
+
+def getApplicationIDsFromAPI(fromDate, toDate): # 200 day max. Epoch time stamp
 
     newCSVFile = {}
 
     # Basic Auth Url with login values as username and password
-    url = "url"
+    url = "https url"
     username = "username"
     password = "password"
     headers = {'accept': 'application/json'}
-    parameters = {'fromDate': '1539131307000', # 10/10/2018
-                    'toDate': '1556348401000'} # 04/27/2019
+    parameters = {'fromDate': fromDate,
+                    'toDate': toDate }
 
     # call the API
     authValues = (username, password)
@@ -30,201 +38,13 @@ def getApplicationIDsFromAPI(): # 200 day max
     data = response.json()
 
     # prepare to convert to csv
-    index = 0
-    while index < data['tot']:
+    for index in range(0, data['tot']):
         id = data['txn'][index]['tid']
         newCSVFile[id] = data['txn'][index]
-        index += 1
 
-    createCSVFromAPIAppID(newCSVFile, parameters)
+    createCSVFromAPIAppID(newCSVFile)
 
-def getApplicationIDsFromCSV(csvFile):
-
-    applicationIDDict = {}
-
-    # parse the csv file to get a list of applicationIDs
-    with open(csvFile) as csv_file:
-        readCSV = csv.reader(csv_file, delimiter=',')
-        count = 0
-
-        for row in readCSV:
-
-            # # prevent "Application ID" header and closed applications from being added
-            if count != 0:
-
-                # column 1 is the application id column in the csv file
-                id = row[0]
-                decision = row[2]
-
-                # add all the app ids that are accepted, rejected, or under review
-                applicationIDDict[id] = decision
-
-            else:
-                count += 1
-
-    callAPIWithDict(applicationIDDict)
-
-def scrubWebsite():
-
-    # create a login session
-    session = requests.session()
-
-    # get the login page
-    loginURL = 'url'
-    login = session.get(loginURL)
-    loginHTML = html.fromstring(login.text)
-
-    # get the CSRF token
-    hiddenInputs = loginHTML.xpath(r'//form//input[@type="hidden"]')
-    form = {x.attrib["name"]: x.attrib["id"] for x in hiddenInputs}
-    form['username'] = 'username'
-    form['password'] = 'password'
-    # csrftoken = login.cookies['csrftoken']
-    # print(login.cookies)
-    print(form)
-
-    # login
-    response = session.post(loginURL, data = form, headers = dict(referer = loginURL))
-    print(response.url)
-    print("" in response.text)
-
-    newURL = "url"
-    newSession = requests.session()
-    test = newSession.get(newURL)
-    print(test.text)
-
-def callAPIWithID(id):
-
-    newCSVFile = {}
-
-    # Basic Auth Url with login values as username and password
-    url = "url"
-    username = "username"
-    password = "password"
-    headers = {'accept': 'application/json'}
-
-    # use the list of application IDs to get information on each one
-    parameters = {'mid': id}
-
-    # Make a request to the endpoint using the correct auth values
-    authValues = (username, password)
-    response = requests.get(url, auth = authValues, headers = headers, params = parameters)
-
-    # Convert JSON to dictionary (de serialize)
-    data = response.json()
-    newCSVFile[id] = data
-
-    print(data)
-    # using the new list of customers, extract only what we need
-    createCSV(newCSVFile)
-
-def callAPIWithDict(applicationIDDict):
-
-    newCSVFile = {}
-    applicationIDs = applicationIDDict.keys()
-
-    # Basic Auth Url with login values as username and password
-    url = "url"
-    username = "username"
-    password = "password"
-    headers = {'accept': 'application/json'}
-
-    # use the list of application IDs to get information on each one
-    for id in applicationIDs:
-
-        parameters = {'mid': id}
-
-        # Make a request to the endpoint using the correct auth values
-        authValues = (username, password)
-        response = requests.get(url, auth = authValues, headers = headers, params = parameters)
-
-        # Convert JSON to dictionary (de serialize)
-        data = response.json()
-        newCSVFile[id] = data
-        newCSVFile[id]['txn'][0]['decision'] = applicationIDDict[id]
-
-    # using the new list of customers, extract only what we need
-    createCSVFromCSVID(newCSVFile)
-
-def createCSVFromCSVID(newCSVFile):
-
-    # open a file for writing
-    customerData = open('Customer List.csv', 'w')
-
-    # create the csv writer object for the file we just opened
-    csvwriter = csv.writer(customerData)
-
-    # grab the application ids
-    tids = newCSVFile.keys()
-    count = 0
-
-    # create this list so we can count how many customers have been accepted
-    decisionList = {
-        'APPROVED': 0,
-        'DECLINED': 0,
-        'REVIEW': 0,
-        'CLOSED': 0
-    }
-
-    for tid in tids:
-
-        # get the information for the specified tid
-        customerDetails = newCSVFile[tid]
-
-        # only add accepted to the form and remove BOs
-        if customerDetails['txn'][0]['decision'] == 'APPROVED' and '@' in customerDetails['txn'][0]['man']:
-
-            # create a dictionary for the fields we want
-            legalName = ''
-
-                # it is a merchant
-            if 'bfn' in customerDetails['txn'][0]:
-                legalName = customerDetails['txn'][0]['bfn'] + ' ' + customerDetails['txn'][0]['bln']
-
-                # it is a consumer
-            else:
-                legalName = customerDetails['txn'][0]['man']
-
-            importantInformation = {
-                # 'Application ID' : tid,
-                'Legal Name': legalName,
-                'man': customerDetails['txn'][0]['man'],
-                'Date Opened': datetime.utcfromtimestamp(int(customerDetails['txn'][0]['tti'])/1000).strftime('%m-%d-%Y %H:%M:%S'),
-                'Decision': customerDetails['txn'][0]['decision'],
-                'Amount' : customerDetails['txn'][0]['amt']
-            }
-
-            # add to our decisionList
-            decisionList[importantInformation['Decision']] += 1
-
-            # create headers if they don't exist yet
-            if count == 0:
-
-                # info for normal header
-                header = list(importantInformation)
-
-                # info for header for findings
-                header.append('Count')
-
-                # create header
-                csvwriter.writerow(header)
-                count += 1
-
-            # add the row values
-            csvwriter.writerow(importantInformation.values())
-
-    # make a new row for our findings
-    decisionListKeys = decisionList.keys()
-    decisions = ''
-    for decision in decisionListKeys:
-        decisions += f'{decision} = {decisionList[decision]}\n'
-    findings = [decisions]
-    csvwriter.writerow(findings)
-
-    # close and save the new csv file to the computer
-    customerData.close()
-
-def createCSVFromAPIAppID(newCSVFile, parameters):
+def createCSVFromAPIAppID(newCSVFile):
 
     # open a file for writing
     customerData = open('Customer List.csv', 'w')
@@ -315,10 +135,170 @@ def createCSVFromAPIAppID(newCSVFile, parameters):
     # close and save the new csv file to the computer
     customerData.close()
 
+# using a csv file
+
+def getApplicationIDsFromCSV(csvFile):
+
+    applicationIDDict = {}
+    newCSVFile = {}
+
+    # parse the csv file to get a list of applicationIDs
+    with open(csvFile) as csv_file:
+        readCSV = csv.reader(csv_file, delimiter=',')
+        count = 0
+
+        for row in readCSV:
+
+            # prevent "Application ID" header and closed applications from being added
+            if count != 0:
+
+                # column 1 is the application id column in the csv file
+                id = row[0]
+                decision = row[2]
+
+                # add all the app ids that are accepted, rejected, or under review
+                applicationIDDict[id] = decision
+
+            else:
+                count += 1
+
+    for id in applicationIDDict:
+        newCSVFile[id] = callAPIWithSingleID(id)
+
+    createCSVFromCSVID(newCSVFile)
+
+def callAPIWithSingleID(id):
+
+    # Basic Auth Url with login values as username and password
+    url = "url"
+    username = "username"
+    password = "password"
+    headers = {'accept': 'application/json'}
+
+    # use the list of application IDs to get information on each one
+    parameters = {'mid': id}
+
+    # Make a request to the endpoint using the correct auth values
+    authValues = (username, password)
+    response = requests.get(url, auth = authValues, headers = headers, params = parameters)
+
+    # Convert JSON to dictionary (de serialize)
+    data = response.json()
+    return data
+
+def createCSVFromCSVID(newCSVFile):
+
+    # open a file for writing
+    customerData = open('Customer List.csv', 'w')
+
+    # create the csv writer object for the file we just opened
+    csvwriter = csv.writer(customerData)
+
+    # grab the application ids
+    tids = newCSVFile.keys()
+    count = 0
+
+    # create this list so we can count how many customers have been accepted
+    decisionList = {
+        'APPROVED': 0,
+        'DECLINED': 0,
+        'REVIEW': 0,
+        'CLOSED': 0
+    }
+
+    for tid in tids:
+
+        # get the information for the specified tid
+        customerDetails = newCSVFile[tid]
+
+        # only add accepted to the form and remove BOs
+        if customerDetails['txn'][0]['decision'] == 'APPROVED' and '@' in customerDetails['txn'][0]['man']:
+
+            # create a dictionary for the fields we want
+            legalName = ''
+
+                # it is a merchant
+            if 'bfn' in customerDetails['txn'][0]:
+                legalName = customerDetails['txn'][0]['bfn'] + ' ' + customerDetails['txn'][0]['bln']
+
+                # it is a consumer
+            else:
+                legalName = customerDetails['txn'][0]['man']
+
+            importantInformation = {
+                # 'Application ID' : tid,
+                'Legal Name': legalName,
+                'man': customerDetails['txn'][0]['man'],
+                'Date Opened': datetime.utcfromtimestamp(int(customerDetails['txn'][0]['tti'])/1000).strftime('%m-%d-%Y %H:%M:%S'),
+                'Decision': customerDetails['txn'][0]['decision'],
+                'Amount' : customerDetails['txn'][0]['amt']
+            }
+
+            # add to our decisionList
+            decisionList[importantInformation['Decision']] += 1
+
+            # create headers if they don't exist yet
+            if count == 0:
+
+                # info for normal header
+                header = list(importantInformation)
+
+                # info for header for findings
+                header.append('Count')
+
+                # create header
+                csvwriter.writerow(header)
+                count += 1
+
+            # add the row values
+            csvwriter.writerow(importantInformation.values())
+
+    # make a new row for our findings
+    decisionListKeys = decisionList.keys()
+    decisions = ''
+    for decision in decisionListKeys:
+        decisions += f'{decision} = {decisionList[decision]}\n'
+    findings = [decisions]
+    csvwriter.writerow(findings)
+
+    # close and save the new csv file to the computer
+    customerData.close()
+
+# other
+
+def scrubWebsite():
+
+    # create a login session
+    session = requests.session()
+
+    # get the login page
+    loginURL = 'url'
+    login = session.get(loginURL)
+    loginHTML = html.fromstring(login.text)
+
+    # get the CSRF token
+    hiddenInputs = loginHTML.xpath(r'//form//input[@type="hidden"]')
+    form = {x.attrib["name"]: x.attrib["id"] for x in hiddenInputs}
+    form['username'] = 'username'
+    form['password'] = 'password'
+    # csrftoken = login.cookies['csrftoken']
+    # print(login.cookies)
+    print(form)
+
+    # login
+    response = session.post(loginURL, data = form, headers = dict(referer = loginURL))
+    print(response.url)
+    print("" in response.text)
+
+    newURL = "url"
+    newSession = requests.session()
+    test = newSession.get(newURL)
+    print(test.text)
+
 def createJSON():
 
     # save json to computer. https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/
     with open('data.txt', 'w') as outfile:
         json.dumps(data, outfile)
 
-getApplicationIDsFromAPI()
+getApplicationIDsFromAPI('1539131307000', '1556348401000') # from 10/10/2018 to 04/27/2019
--- Customer Analysis (Python)/transactionMonitoring.py
+++ Customer Analysis (Python)/transactionMonitoring.py
@@ -7,10 +7,19 @@ from lxml import html               # to parse html
 from bs4 import BeautifulSoup       # to parse html
 
 
-# download csv file. run it through the script, and you should have a csv file back with new
-# information that you can view with excel
+'''
+you can run the script in 1 of 2 ways. both return a CSV file for additional analysis
+1) use an HTTPS request. use getApplicationIDsFromAPI()
+2) use a CSV file. use getApplicationIDsFromCSV()
 
-def getApplicationIDsFromAPI(fromDate, toDate): # 200 day max
+note: using an HTTPS request w/ a single ID vs using a time frame
+returns different JSON responses
+
+'''
+
+# using an HTTPS request
+
+def getApplicationIDsFromAPI(fromDate, toDate, totalTransactionReport): # 200 day max. epoch time
 
     newCSVFile = {}
 
@@ -30,145 +39,13 @@ def getApplicationIDsFromAPI(fromDate, toDate): # 200 day max
     data = response.json()
 
     # prepare to convert to csv
-    index = 0
-    while index < data['tot']:
+    for index in range(0, data['tot']):
         id = data['txn'][index]['tid']
         newCSVFile[id] = data['txn'][index]
-        index += 1
 
     createCSVFromAPIAppID(newCSVFile)
-    # totalTransactionReport(newCSVFile)
-
-def getApplicationIDsFromCSV(csvFile):
-
-    applicationIDDict = {}
-
-    # parse the csv file to get a list of applicationIDs
-    with open(csvFile) as csv_file:
-        readCSV = csv.reader(csv_file, delimiter=',')
-        count = 0
-
-        for row in readCSV:
-
-            # # prevent "Application ID" header and closed applications from being added
-            if count != 0:
-
-                # column 1 is the application id column in the csv file
-                id = row[0]
-                type = row[4]
-
-
-                # add all the app ids that are accepted, rejected, or under review
-                applicationIDDict[id] = type
-
-            else:
-                count += 1
-
-    callAPIWithDict(applicationIDDict)
-
-def callAPIWithID(id):
-
-    newCSVFile = {}
-
-    # Basic Auth Url with login values as username and password
-    url = "url"
-    username = "username"
-    password = "password"
-    headers = {'accept': 'application/json'}
-
-    # use the list of application IDs to get information on each one
-    parameters = {'mid': id}
-
-    # Make a request to the endpoint using the correct auth values
-    authValues = (username, password)
-    response = requests.get(url, auth = authValues, headers = headers, params = parameters)
-
-    # Convert JSON to dictionary (de serialize)
-    data = response.json()
-    newCSVFile[id] = data
-
-    # using the new list of customers, extract only what we need
-    createCSV(newCSVFile)
-
-def callAPIWithDict(applicationIDs):
-
-    newCSVFile = {}
-
-    # Basic Auth Url with login values as username and password
-    url = "url"
-    username = "username"
-    password = "password"
-    headers = {'accept': 'application/json'}
-
-    # use the list of application IDs to get information on each one
-    for id in applicationIDs:
-
-        parameters = {'mid': id}
-
-        # Make a request to the endpoint using the correct auth values
-        authValues = (username, password)
-        response = requests.get(url, auth = authValues, headers = headers, params = parameters)
-
-        # Convert JSON to dictionary (de serialize)
-        data = response.json()
-        newCSVFile[id] = data
-
-    # using the new list of customers, extract only what we need
-    createCSV(newCSVFile)
-
-def createCSV(newCSVFile):
-
-    # open a file for writing
-    customerData = open('Transaction Monitoring.csv', 'w')
-
-    # create the csv writer object for the file we just opened
-    csvwriter = csv.writer(customerData)
-
-    # grab the application ids
-    tids = newCSVFile.keys()
-    count = 0
-
-    # create a dictionary of customers to count number of times they transact
-    customerList = {}
-
-    for tid in tids:
-
-        # get the information for the specified tid
-        customerDetails = newCSVFile[tid]
-
-        # create a dictionary for the fields we want
-        name = 'Unknown'
-        if ('man' in customerDetails['txn'][0]):
-            name = customerDetails['txn'][0]['man']
-
-        transactionType = 'Funding' # USD in to us
-        if (customerDetails['txn'][0]['tt'] == 'AccountTransferOut'):
-            transactionType = "Withdrawal" # sell USD to them
-
-        importantInformation = {
-            'man': name,
-			'Amount': '{:,}'.format(customerDetails['txn'][0]['amt']),
-            'Currency': customerDetails['txn'][0]['ccy'],
-			'Transaction Time': datetime.utcfromtimestamp(int(customerDetails['txn'][0]['tti'])/1000).strftime('%m-%d-%Y %H:%M:%S'),
-            'Transaction Type': transactionType,
-            'Decision': customerDetails['txn'][0]['decision']
-		}
-
-        # create headers if they don't exist yet
-        if count == 0:
-
-            # info for normal header
-            header = list(importantInformation)
-
-            # create header
-            csvwriter.writerow(header)
-            count += 1
-
-        # add the row values
-        csvwriter.writerow(importantInformation.values())
-
-    # close and save the new csv file to the computer
-    customerData.close()
+    if totalTransactionReport:
+        totalTransactionReport(newCSVFile)
 
 def createCSVFromAPIAppID(newCSVFile):
 
@@ -228,6 +105,114 @@ def createCSVFromAPIAppID(newCSVFile):
 
     # close and save the new csv file to the computer
     customerData.close()
+
+# using a csv file
+
+def getApplicationIDsFromCSV(csvFile):
+
+    applicationIDDict = {}
+    newCSVFile = {}
+
+    # parse the csv file to get a list of applicationIDs
+    with open(csvFile) as csv_file:
+        readCSV = csv.reader(csv_file, delimiter=',')
+        count = 0
+
+        for row in readCSV:
+
+            # prevent "Application ID" header and closed applications from being added
+            if count != 0:
+
+                # column 1 is the application id column in the csv file
+                id = row[0]
+                type = row[4]
+
+
+                # add all the app ids that are accepted, rejected, or under review
+                applicationIDDict[id] = type
+
+            else:
+                count += 1
+
+    for id in applicationIDDict:
+        newCSVFile[id] = callAPIWithSingleID(id)
+
+    createCSVFromCSVID(newCSVFile)
+
+def callAPIWithSingleID(id):
+
+    # Basic Auth Url with login values as username and password
+    url = "url"
+    username = "username"
+    password = "password"
+    headers = {'accept': 'application/json'}
+
+    # use the list of application IDs to get information on each one
+    parameters = {'mid': id}
+
+    # Make a request to the endpoint using the correct auth values
+    authValues = (username, password)
+    response = requests.get(url, auth = authValues, headers = headers, params = parameters)
+
+    # Convert JSON to dictionary (de serialize)
+    data = response.json()
+    return data
+
+def createCSVFromCSVID(newCSVFile):
+
+    # open a file for writing
+    customerData = open('Transaction Monitoring.csv', 'w')
+
+    # create the csv writer object for the file we just opened
+    csvwriter = csv.writer(customerData)
+
+    # grab the application ids
+    tids = newCSVFile.keys()
+    count = 0
+
+    # create a dictionary of customers to count number of times they transact
+    customerList = {}
+
+    for tid in tids:
+
+        # get the information for the specified tid
+        customerDetails = newCSVFile[tid]
+
+        # create a dictionary for the fields we want
+        name = 'Unknown'
+        if ('man' in customerDetails['txn'][0]):
+            name = customerDetails['txn'][0]['man']
+
+        transactionType = 'Funding' # USD in to us
+        if (customerDetails['txn'][0]['tt'] == 'AccountTransferOut'):
+            transactionType = "Withdrawal" # sell USD to them
+
+        importantInformation = {
+            'man': name,
+			'Amount': '{:,}'.format(customerDetails['txn'][0]['amt']),
+            'Currency': customerDetails['txn'][0]['ccy'],
+			'Transaction Time': datetime.utcfromtimestamp(int(customerDetails['txn'][0]['tti'])/1000).strftime('%m-%d-%Y %H:%M:%S'),
+            'Transaction Type': transactionType,
+            'Decision': customerDetails['txn'][0]['decision']
+		}
+
+        # create headers if they don't exist yet
+        if count == 0:
+
+            # info for normal header
+            header = list(importantInformation)
+
+            # create header
+            csvwriter.writerow(header)
+            count += 1
+
+        # add the row values
+        csvwriter.writerow(importantInformation.values())
+
+    # close and save the new csv file to the computer
+    customerData.close()
+
+# other
 
 def totalTransactionReport(newCSVFile):
 
@@ -295,5 +280,5 @@ def createJSON():
     with open('data.txt', 'w') as outfile:
         json.dumps(data, outfile)
 
-getApplicationIDsFromAPI('1553562000000', '1553648400000') # 03/25/2019 6 PM PST, 03/26/2019 6 PM PST // https://www.epochconverter.com/
+getApplicationIDsFromAPI('1553562000000', '1553648400000', False) # 03/25/2019 6 PM PST, 03/26/2019 6 PM PST // https://www.epochconverter.com/
                                                             # '1539131307000', 1556348401000 # for 09/01/18 - 04/27/19
